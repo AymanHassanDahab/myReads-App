5d088bb88adee319708424ba6d69df0acd74aa99
AymanHassanDahab <66025405+AymanHassanDahab@users.noreply.github.com>
2023-01-22 12:13:38 +0200

    Update App.js

--- starter/src/App.js
+++ starter/src/App.js
@@ -9,50 +9,48 @@ function App() {
   // Defining Books State 
   const [books, setBooks] = useState([])
   // Receiving Books Data from BooksAPI and updating the books state with it via useEffect Hook
-    useEffect(() => {
+  useEffect(() => {
       const getBooks = async () => {
         await BooksAPI.getAll().then((allBooks) => setBooks(allBooks));
       }   
       getBooks();
-    }, [])
+  }, []);
 
   // Update Shelf Of Books Category
   const updateShelf = (bookToChange, newShelf) => { 
-    setBooks( 
-      books.map(b => {
-        if (b.id === bookToChange.id) {
-          bookToChange.shelf = newShelf;
-          return bookToChange;
-        } else {
-          return b;
-        }
-      })
-    );
-    BooksAPI.update(bookToChange, newShelf)
+      setBooks( 
+          books.map(b => {
+              if (b.id === bookToChange.id) {
+                bookToChange.shelf = newShelf;
+                return bookToChange;
+              } else {
+                return b;
+              }
+          })
+      );
+      BooksAPI.update(bookToChange, newShelf)
   }
   
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={
-        <div className="list-books">
-          {/* HEADER */} 
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>   
-
-          {/* SHELVES */} 
-          <Shelves booksFromAPI={books} updateBookShelf={updateShelf} />
-
-          {/* SEARCH BUTTON */} 
-          <div className="open-search">
-              <Link className="open-search" to="/SearchPage"></Link>
-          </div>
-        </div>
-        } />
-        
-        <Route path='/SearchPage' element={<SearchPage updateBookShelf={updateShelf} booksFromAPI={books} />} />
-      </Routes>
+        <Routes>
+            <Route path="/" element={
+                <div className="list-books">
+                    {/* HEADER */} 
+                    <div className="list-books-title">
+                        <h1>MyReads</h1>
+                    </div>   
+                    {/* SHELVES */} 
+                    <Shelves booksFromAPI={books} updateBookShelf={updateShelf} />
+                    {/* SEARCH BUTTON */} 
+                    <div className="open-search">
+                        <Link className="open-search" to="/SearchPage"></Link>
+                    </div>
+                </div>
+            } />
+            
+            <Route path='/SearchPage' element={<SearchPage updateBookShelf={updateShelf} booksFromAPI={books} />} />
+        </Routes>
     </div>
   );
 }
