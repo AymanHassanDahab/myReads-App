ca022ca0ffe7df5f7ce1f31d5bd3ac786faf5bb9
AymanHassanDahab <66025405+AymanHassanDahab@users.noreply.github.com>
2023-01-22 12:07:37 +0200

    Update SearchPage.js

--- starter/src/components/SearchPage.js
+++ starter/src/components/SearchPage.js
@@ -1,76 +1,71 @@
 import React from 'react'
 import { useState , useEffect} from "react";
 import { Link } from "react-router-dom";
-import Book from './Book'
 import * as BooksAPI from '../BooksAPI'
-
+import Book from './Book'
 
 const SearchPage = ({updateBookShelf, booksFromAPI}) => {
-  // Defining Search Query State
-  const [query, setQuery] = useState("");
-  const [searchedBook, setSearchedBook] = useState([]);
-  // Receiving Query Data from BooksAPI via useEffect Hook
-  useEffect(() => {
-    let active = true;
-    const searchOutput = async () => {
-        if (query) {
-            const dataOutput = await BooksAPI.search(query)
-            if (dataOutput.error) {
-                setSearchedBook([])
-            } else if (active) {              
-                setSearchedBook(dataOutput.map(setSearchedBook => {
-                        booksFromAPI.forEach((book)=>
-                        {
-                            if (searchedBook.title === book.title) {
-                                searchedBook.shelf = book.shelf
-                            }                                  
-                        });
-                        return setSearchedBook
-                    })
-                )        
+    // Defining "Queries" State
+    const [query, setQuery] = useState("");
+    // Defining "searched books" State
+    const [searchedBook, setSearchedBook] = useState([]);
+    // Receiving Query Data from BooksAPI via useEffect Hook
+    useEffect(() => {
+        const searchOutput = async () => {
+            if (query) {
+                await BooksAPI.search(query).then((dataOutput) => {
+                    if (dataOutput.error) {
+                        setSearchedBook([])
+                    } else {              
+                        setSearchedBook(dataOutput.map(setSearchedBook => {
+                            booksFromAPI.forEach((book) => {
+                                if (searchedBook.title === book.title) {
+                                    searchedBook.shelf = book.shelf
+                                }                                  
+                            });
+                            return setSearchedBook
+                        })
+                    )}
+                })   
             }
+        } 
+        if (query !== "" ) {
+            searchOutput();
+        } else if (!searchedBook.shelf) {
+            searchedBook.shelf = "none";
         }
-    } 
-    if (query !== "" ) {
-        searchOutput();
-    } else if (!searchedBook.shelf) {
-        searchedBook.shelf = "none";
-    }
-    return() => {
-      active = false;
-      setSearchedBook([]);
-    }
-  }, [query])
+        return() => {
+            setSearchedBook([]);
+        }
+    }, [query])
 
-  return (
-    <div className="search-books">
-        <div className="search-books-bar">
-            <Link to="/">
-                <button className="close-search" >
-                    Close
-                </button>
-            </Link>
-            <div className="search-books-input-wrapper">
-                <input 
-                type="text" 
-                placeholder="Search by title, author, or ISBN" 
-                value={query} 
-                onChange={(e) => {setQuery(e.target.value)}} 
-                />
+    return (
+        <div className="search-books">
+            <div className="search-books-bar">
+                <Link to="/">
+                    <button className="close-search" >Close</button>
+                </Link>
+                <div className="search-books-input-wrapper">
+                    <input 
+                    type="text" 
+                    placeholder="Search by title, author, or ISBN" 
+                    value={query} 
+                    onChange={(e) => {setQuery(e.target.value)}} 
+                    />
+                </div>
+            </div>
+            <div className="search-books-results">
+                <ol className="books-grid">
+                    { 
+                    searchedBook.map((selectedBook) => 
+                        <li key={selectedBook.id}>
+                            <Book bookCategory={selectedBook} changeBookShelf={updateBookShelf} />
+                        </li>              
+                    )}
+                </ol>
             </div>
         </div>
-        <div className="search-books-results">
-            <ol className="books-grid">
-                { 
-                searchedBook.map((b) => 
-                    <li key={b.id}>
-                        <Book bookCategory={b} changeBookShelf={updateBookShelf} />
-                    </li>              
-                )}
-            </ol>
-        </div>
-    </div>
-  )
+    );
 }
 
 export default SearchPage
