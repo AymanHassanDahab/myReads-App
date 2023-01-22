a7d5d16f19f9b2bb2422dc59ed174ef5ec29ba99
AymanHassanDahab <66025405+AymanHassanDahab@users.noreply.github.com>
2023-01-22 12:05:40 +0200

    Update App.js

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,39 +1,36 @@
 import "./App.css";
 import { useState , useEffect} from "react";
 import { Routes, Route ,Link} from 'react-router-dom';
+import * as BooksAPI from './BooksAPI'
 import SearchPage from "./components/SearchPage";
 import Shelves from "./components/Shelves";
-import * as BooksAPI from './BooksAPI'
 
 function App() {
-  // Defining viewed page state
-  const [showSearchPage, setShowSearchPage] = useState(false);
-
   // Defining Books State 
   const [books, setBooks] = useState([])
   // Receiving Books Data from BooksAPI and updating the books state with it via useEffect Hook
     useEffect(() => {
       const getBooks = async () => {
-        const res = await BooksAPI.getAll();
-        setBooks(res); 
+        await BooksAPI.getAll().then((allBooks) => setBooks(allBooks));
       }   
       getBooks();
     }, [])
 
   // Update Shelf Of Books Category
   const updateShelf = (bookToChange, newShelf) => { 
-  const updatedBook = books.map(b => {
-    if (b.id === bookToChange.id) {
-      bookToChange.shelf = newShelf;
-      return bookToChange;
-    }
-    return b;
-  })
-  setBooks(updatedBook);
-  BooksAPI.update(bookToChange, newShelf)
+    setBooks( 
+      books.map(b => {
+        if (b.id === bookToChange.id) {
+          bookToChange.shelf = newShelf;
+          return bookToChange;
+        } else {
+          return b;
+        }
+      })
+    );
+    BooksAPI.update(bookToChange, newShelf)
   }
-
-
+  
   return (
     <div className="app">
       <Routes>
@@ -53,6 +50,7 @@ function App() {
           </div>
         </div>
         } />
+        
         <Route path='/SearchPage' element={<SearchPage updateBookShelf={updateShelf} booksFromAPI={books} />} />
       </Routes>
     </div>
